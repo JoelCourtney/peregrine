6a68396d128292124b1a775d510ec66a21c491f9
JoelCourtney <joel.e.courtney@gmail.com>
2026-02-06 19:10:48 -0700

    Use jemalloc for perf

--- perf/src/main.rs
+++ perf/src/main.rs
@@ -1,5 +1,16 @@
 use clap::Parser;
-use peregrine::{op, plan::{Duration, Resource, Time}, run};
+use peregrine::{
+    op,
+    plan::{Duration, Resource, Time},
+    run,
+};
+
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
 
 #[derive(Parser)]
 struct Args {
@@ -10,18 +21,18 @@ fn main() {
     let args = Args::parse();
 
     println!("Running {} operations", args.num_ops);
-    
+
     let mut time = Time::from_tai_seconds(0.0);
-    
+
     let x = Resource::new(1);
-    
+
     for _ in 0..args.num_ops {
         time += Duration::from_seconds(1.0);
         x.mutate_at(time, |x| op! { i!(x) + 1 });
-        
+
         time += Duration::from_seconds(1.0);
         // y.mutate_at(time, |y| op! { i!(y) + 1 });
     }
-    
+
     println!("x: {}", run(x.get_at_inc(time)));
 }
